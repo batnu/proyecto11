03a006b8fb83eced352666ca5b37327aaef6d1f0
batnu <batnu_piro@hotmail.com>
2019-10-10 14:20:58 +0000

    segundo commit

--- public/tema6/index.php
+++ public/tema6/index.php
@@ -6,24 +6,43 @@
 </head>
 <body>
 	<h1>Formulario usables</h1>
-	<?php if (! $_POST) { 
+	<?php 
+	function showErrors($errors){
+		echo '<ul>';
+		foreach ($errors as $error) {
+			echo '<li>'.$error.'</li>';
+		}
+		echo '</ul>';
+	}
+
+	if (! $_POST) { 
 		include 'formulario.php';
 	}else {
+		$errors = [];
 		if (! isset($_POST['name'])) {
-			echo "No he recibido el nombre";
-		}elseif (strlen($_POST['name'])<3) {
-			echo "El nombre es demasiado corto";
-		}elseif (! isset($_POST['email'])) {
-			echo "No he recibido el email";
-		}elseif (strlen($_POST['email'])<6) {
-			echo "El correo electrónico no es válido";
-		}elseif (! isset($_POST['password'])||!isset($_POST['password2'])) {
-			echo "No he recibido las dos contraseñas";
-		}elseif (strlen($_POST['password'])<6) {
-			echo "La contraseña debe tener al menos 6 caracteres";
-		}elseif ($_POST['password'] != $_POST['password2']) {
-			echo "Las contraseñas no coinciden";
-		}else{
+			$errors[] = "No he recibido el nombre";
+		}
+		if (strlen($_POST['name'])<3) {
+			$errors[] = "El nombre es demasiado corto";
+		}
+		if (! isset($_POST['email'])) {
+			$errors[] = "No he recibido el email";
+		}
+		if (strlen($_POST['email'])<6) {
+			$errors[] = "El correo electrónico no es válido";
+		}
+		if (! isset($_POST['password'])||!isset($_POST['password2'])) {
+			$errors[] = "No he recibido las dos contraseñas";
+		}if (strlen($_POST['password'])<6) {
+			$errors[] = "La contraseña debe tener al menos 6 caracteres";
+		}
+		if ($_POST['password'] != $_POST['password2']) {
+			$errors[] = "Las contraseñas no coinciden";
+		}
+		if ($errors) {
+			showErrors($errors);
+			echo '<br/><a href="index.php"> volver al formulario</a>';		}
+		else{			
 			echo "Todo bien, pasamos a registrar ese usuario";
 		}
 	} ?>
